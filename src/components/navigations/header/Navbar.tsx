29ebddfad7e5c7219b53c1168d44b5b10f1cee4e
marcellandreas <marcellandreas.123@gmail.com>
2025-11-22 22:48:01 +0700

    refactor style

--- src/components/navigations/header/Navbar.tsx
+++ src/components/navigations/header/Navbar.tsx
@@ -1,3 +1,4 @@
+
 import './style.scss'
 import { useState, useEffect } from 'react'
 import { useDebounce } from '../../../hooks/useDebounce'
@@ -8,7 +9,6 @@ import type { Movie } from '../../../types'
 import { AxiosInstance } from '../../../apis/api'
 import Search from '../../search/search'
 
-
 const Navbar: React.FC = () => {
   const [active, setActive] = useState(false)
   const [mobile, setMobile] = useState(false)
@@ -18,74 +18,73 @@ const Navbar: React.FC = () => {
   const [isSearching, setIsSearching] = useState(false)
 
   const changeBackground = () => {
-    if (window.scrollY >= 80) {
-      setActive(true)
-    } else {
-      setActive(false)
-    }
+    setActive(window.scrollY >= 80)
   }
-
-  useEffect(() => {
-    const searchAPI = async () => {
-      setIsSearching(true)
-      try {
-        const response = await AxiosInstance.get(`search/movie?query=${debouncedQuery}&`, {
-          params: { api_key: import.meta.env.VITE_TMBD_KEY },
-        })
-        setDataQueryMovie(response.data.results)
-      } catch (error) {
-        setDataQueryMovie([])
-      } finally {
-        setIsSearching(false)
-      }
-    }
-
-    if (debouncedQuery) {
-      searchAPI()
-    } else {
-      setDataQueryMovie([])
-      setIsSearching(false)
-    }
-  }, [debouncedQuery])
 
   useEffect(() => {
     window.addEventListener('scroll', changeBackground)
     return () => window.removeEventListener('scroll', changeBackground)
   }, [])
 
+  useEffect(() => {
+    const searchAPI = async () => {
+      setIsSearching(true)
+      try {
+        const response = await AxiosInstance.get(
+          `search/movie?query=${debouncedQuery}&`,
+          { params: { api_key: import.meta.env.VITE_TMBD_KEY } }
+        )
+        setDataQueryMovie(response.data.results)
+      } catch {
+        setDataQueryMovie([])
+      } finally {
+        setIsSearching(false)
+      }
+    }
+
+    if (debouncedQuery) searchAPI()
+    else {
+      setDataQueryMovie([])
+      setIsSearching(false)
+    }
+  }, [debouncedQuery])
+
   return (
     <nav className={active ? 'navbar__container active' : 'navbar__container'}>
-      <div className="logo__navbar">M-Streaming</div>
-      <div
-        className={mobile ? 'list_menu_mobile' : 'list_menu'}
-        onClick={() => setMobile(false)}
-      >
+      <div className="logo__navbar">M‑Streaming</div>
+
+      {/* Desktop Menu */}
+      <div className={mobile ? 'list_menu_mobile' : 'list_menu'} onClick={() => setMobile(false)}>
         <div className="menu__navbar">
           <Link to="#" className="Link color_nav">Movies</Link>
           <Link to="#" className="Link color_nav">TV Show</Link>
           <Link to="#" className="Link color_nav">Rekomendasi</Link>
         </div>
       </div>
+
+      {/* Search */}
       <div className="search__film">
         <MdSearch className="icon__navbar" />
         <input
           type="text"
           className="input__search"
+          placeholder="Search movies..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+
         {isSearching && (
           <span className="search-loading" aria-hidden>
-            <svg width="18" height="18" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-              <path fill="currentColor" d="M43.935,25.145c0-10.318-8.364-18.681-18.68-18.681c-10.317,0-18.68,8.363-18.68,18.681h4.068
-                c0-8.059,6.553-14.612,14.612-14.612c8.06,0,14.613,6.553,14.613,14.612H43.935z">
-                <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.9s" repeatCount="indefinite"/>
-              </path>
+            <svg width="18" height="18" viewBox="0 0 50 50">
+              <path fill="currentColor" d="M43.935,25.145c0-10.318-8.364-18.681-18.68-18.681..."></path>
             </svg>
           </span>
         )}
+
         {dataQueryMovie.length > 0 && <Search data={dataQueryMovie} />}
       </div>
+
+      {/* Mobile Menu */}
       <button className="mobile-menu" onClick={() => setMobile(!mobile)}>
         {mobile ? <MdClose className="icon__navbar" /> : <GiHamburgerMenu className="icon__navbar" />}
       </button>
@@ -94,3 +93,4 @@ const Navbar: React.FC = () => {
 }
 
 export default Navbar
+
