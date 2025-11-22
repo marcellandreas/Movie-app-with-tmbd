62398b65db4b0d58c818c795f38481413e046530
marcellandreas <marcellandreas.123@gmail.com>
2025-11-22 21:46:09 +0700

    feat(apps): remove params

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import './App.css'
 
 
-import { AxiosInstance } from './apis/api'
+import { AxiosInstance, movieService } from './apis/api'
 import { useAppDispatch, useAppSelector } from './store/hooks'
 import { getGenres, setDataFilmComingSoon, setDataFilmNowPlaying, setDataPopular } from './features/dataFilmSlice'
 import Routing from './routes'
@@ -15,36 +15,31 @@ const App: React.FC = () => {
   const { page } = useAppSelector((state) => state.dataFilm.counterPage)
 
   useEffect(() => {
-    AxiosInstance.get(`/movie/popular?page=${page}&`, {
-      params: { api_key: import.meta.env.VITE_TMBD_KEY },
-    }).then((res) => {
+    AxiosInstance.get(`/movie/popular?page=${page}`).then((res) => {
       dispatch(setDataPopular(res.data))
     })
   }, [dispatch, page])
 
   useEffect(() => {
-    AxiosInstance.get(`/movie/now_playing`, {
-      params: { api_key: import.meta.env.VITE_TMBD_KEY },
-    }).then((res) => {
+    AxiosInstance.get(`/movie/now_playing`).then((res) => {
       dispatch(setDataFilmNowPlaying(res.data.results))
     })
   }, [dispatch, page])
 
   useEffect(() => {
-    AxiosInstance.get(`/movie/upcoming`, {
-      params: { api_key: import.meta.env.VITE_TMBD_KEY },
-    }).then((res) => {
+    AxiosInstance.get(`/movie/upcoming`).then((res) => {
       dispatch(setDataFilmComingSoon(res.data.results))
     })
   }, [dispatch])
 
   useEffect(() => {
-    AxiosInstance.get(`/genre/movie/list`, {
-      params: { api_key: import.meta.env.VITE_TMBD_KEY },
-    }).then((res) => {
+    AxiosInstance.get(`/genre/movie/list`).then((res) => {
       dispatch(getGenres(res.data.genres))
     })
   }, [dispatch])
+
+  
+
 
   return (
      <>
