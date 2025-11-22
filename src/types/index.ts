340c8b4c3ee6202f13ebad8067d42e2ec1b72480
marcellandreas <marcellandreas.123@gmail.com>
2025-11-22 21:42:39 +0700

    feat: add MoviePopular interface to enhance type definitions for popular movies

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,6 +12,24 @@ export interface Movie {
   genre_ids?: number[]
 }
 
+export interface MoviePopular {
+      adult: boolean,
+      backdrop_path: string,
+      genre_ids: number[],
+      id: number,   
+      original_language: string
+      original_title: string
+      overview: string
+      popularity: string
+      poster_path: string
+      release_date: string
+      title: string
+      video: false,
+      vote_average: number,
+      vote_count: number
+    
+}
+
 export interface Genre {
   id: number
   name: string
